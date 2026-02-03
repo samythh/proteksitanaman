b93c750fde44449026bb238c3ad5e3c5b0901be9
samythh <habibillahmikail@gmail.com>
2026-02-03 10:00:09 +0700

    accessibility widget again2

--- src/components/AccessibilityWidget.tsx
+++ src/components/AccessibilityWidget.tsx
@@ -4,16 +4,20 @@
 import Script from "next/script";
 
 export default function AccessibilityWidget() {
-   // --- KONFIGURASI STANDAR ---
    const BASE_URL = "https://www.skynettechnologies.com/accessibility/js/all-in-one-accessibility-js-widget-minify.js";
 
-   const colorCode = "#005320";
+   const colorCode = "FFFF00";
 
    const position = "bottom_right";
 
+   const iconSize = "aioa-extra-small-icon";
+
+   const iconType = "aioaicontype1";
+
+   // Token (null jika free version)
    const token = process.env.NEXT_PUBLIC_AIOA_TOKEN || "null";
 
-   const scriptSrc = `${BASE_URL}?colorcode=${encodeURIComponent(colorCode)}&token=${token}&position=${position}`;
+   const scriptSrc = `${BASE_URL}?colorcode=#${colorCode}&token=${token}&position=${position}.${iconType}&aioa_icon_size=${iconSize}`;
 
    return (
       <Script
